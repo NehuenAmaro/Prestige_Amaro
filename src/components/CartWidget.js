ee38de7d91ef77be5b596f70590c1764085398f2
Nehuen.Amaro <nehuenamaro1@gmail.com>
2022-08-24 19:09:09 -0300

    clase 3 git remote add origin https://github.com/NehuenAmaro/Prestige_Amaro.git git remote -v

---
+++ src/components/CartWidget.js
@@ -0,0 +1,11 @@
+import { MdShoppingCart } from 'react-icons/md';
+
+const CartWidget = () => {
+    return (
+        <>
+        <MdShoppingCart className='size-2 ml-2' id='iconshop'/>
+        </>
+    )
+};
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+
+const ItemListContainer = (props) => {
+    return (
+        <main>
+                <h2 className="text-center size-2 my-1">{props.saludo}</h2>
+        </main>
+    );
+}
+
+export default ItemListContainer;
